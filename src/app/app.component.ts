d3c38fa59fce68aa120b8d70f7101a807363b0fc
Luiz <luizgarcia.devbr@gmail.com>
2026-05-16 16:08:20 -0300

    Adicionar navegação automática para a página de boas-vindas no componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private router: Router) {
+    this.router.navigateByUrl('/welcome', { replaceUrl: true });
+  }
 }
