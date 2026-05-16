8a8f8818dce6865515d33db432dc1d7b54e83462
Luiz <luizgarcia.devbr@gmail.com>
2026-05-16 16:08:51 -0300

    Adicionar páginas de login para estabelecimentos e clientes com formulários e navegação

--- src/app/login-company/login-company.page.ts
+++ src/app/login-company/login-company.page.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonContent } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { IonButton, IonContent, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login-company',
   templateUrl: './login-company.page.html',
   styleUrls: ['./login-company.page.scss'],
   standalone: true,
-  imports: [IonContent],
+  imports: [CommonModule, IonButton, IonContent, IonInput],
 })
-export class LoginCompanyPage {}
+export class LoginCompanyPage {
+  isLogin: boolean = true;
+
+  constructor(private router: Router) {}
+
+  toggleMode(): void {
+    this.isLogin = !this.isLogin;
+  }
+
+  navigateToDashboard(): void {
+    this.router.navigate(['/dashboard']);
+  }
+}
--- src/app/login-user/login-user.page.ts
+++ src/app/login-user/login-user.page.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonContent } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { IonButton, IonContent, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login-user',
   templateUrl: './login-user.page.html',
   styleUrls: ['./login-user.page.scss'],
   standalone: true,
-  imports: [IonContent],
+  imports: [CommonModule, IonButton, IonContent, IonInput],
 })
-export class LoginUserPage {}
+export class LoginUserPage {
+  isLogin: boolean = true;
+
+  constructor(private router: Router) {}
+
+  toggleMode(): void {
+    this.isLogin = !this.isLogin;
+  }
+
+  navigateToHome(): void {
+    this.router.navigate(['/home']);
+  }
+}
